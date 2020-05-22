d8593978fbf81fb1f07423cfcd11b4d1735c35e4
Seife <806017047@qq.com>
2020-05-22 13:43:01 +0800

    Mix two model to one model_C

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import torch
 import numpy as np
 import torch.nn as nn
-from model_ import ModuleA
+from model_ import ModuleA, ModuleB, ModuleC
+import collections
 from torchviz import make_dot
 
-
-def main():
+def A():
     input_A = torch.rand((1, 3, 9, 9)).requires_grad_(True)
     output_A = torch.rand((1, 1)).requires_grad_(True)
     model_A = ModuleA()
@@ -29,13 +29,70 @@ def main():
     # graph.view()
 
     state_dict = model_A.state_dict()
-    stat = {'model_state_dict':state_dict}
+    # print(state_dict['Conv.weight'])
+    stat = {'model_state_dict': state_dict}
     torch.save(stat, 'model_A.pth')
 
-    state = torch.load('model_A.pth', map_location='cpu')
+    # state = torch.load('model_A.pth', map_location='cpu')
+    # print(state['model_state_dict']['Conv.weight'])
+
+
+def B():
+    input_B = torch.rand((1, 3, 3, 3)).requires_grad_(True)
+    output_B = torch.rand((1, 1)).requires_grad_(True)
+    model_B = ModuleB()
+    model_B.train()
+    loss_function = nn.L1Loss()
+    optim = torch.optim.Adam(model_B.parameters(), lr=0.001)
+    output = model_B(input_B)
+    loss = loss_function(output, output_B)
+    optim.zero_grad()
+    loss.backward()
+    optim.step()
+    # graph = make_dot(output.mean(), params=dict(list(model_B.named_parameters()) + [('input', input_B)]))
+    # graph.view()
+
+    input_B = torch.rand((1, 3, 3, 3)).requires_grad_(True)
+    output_B = torch.rand((1, 1)).requires_grad_(True)
+    output = model_B(input_B)
+    loss = loss_function(output, output_B)
+    optim.zero_grad()
+    loss.backward()
+    optim.step()
+
+    state_dict = model_B.state_dict()
+    print(state_dict['Conv.weight'])
+    stat = {'model_state_dict': state_dict}
+    torch.save(stat, 'model_B.pth')
+
+    state = torch.load('model_B.pth', map_location='cpu')
     print(state['model_state_dict']['Conv.weight'])
 
 
+def C(a_path, b_path):
+    input_C = torch.rand((1, 3, 5, 5)).requires_grad_(True)
+    output_C = torch.rand((1, 1)).requires_grad_(True)
+    model_C = ModuleC()
+    output = model_C(input_C)
+    # graph = make_dot(output.mean(), params=dict(list(model_C.named_parameters()) + [('input', input_C)]))
+    # graph.view()
+    a_stat = torch.load(a_path)
+    b_stat = torch.load(b_path)
+    # print('A:', a_stat['model_state_dict'])
+    # print('B:', b_stat['model_state_dict'])
+    mix_stat = collections.OrderedDict()
+    mix_stat['Conv_0.weight'] = a_stat['model_state_dict']['Conv.weight']
+    mix_stat['Conv_0.bias'] = a_stat['model_state_dict']['Conv.bias']
+    mix_stat['Conv_1.weight'] = b_stat['model_state_dict']['Conv.weight']
+    mix_stat['Conv_1.bias'] = b_stat['model_state_dict']['Conv.bias']
+    print(mix_stat)
+    model_C.load_state_dict(mix_stat)
+
+
+def main():
+    # A()
+    # B()
+    C('model_A.pth', 'model_B.pth')
 
 if __name__ == '__main__':
     main()
--- model_.py
+++ model_.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+
 class ModuleA(nn.Module):
     def __init__(self):
         super(ModuleA, self).__init__()
@@ -13,4 +14,30 @@ class ModuleA(nn.Module):
         out = self.Conv(x)
         out = self.Mid_Conv(out)
         out = self.Final_Conv(self.Max_pool(out))
+        return out.squeeze(dim=2).squeeze(dim=2)
+
+
+class ModuleB(nn.Module):
+    def __init__(self):
+        super(ModuleB, self).__init__()
+        self.Conv = nn.Conv2d(in_channels=3, out_channels=1, kernel_size=3, stride=1, padding=1)
+        self.Max_pool = nn.MaxPool2d(kernel_size=3, stride=1)
+
+    def forward(self, x):
+        out = self.Conv(x)# [B, 1, 3, 3]
+        out = self.Max_pool(out) #[B, 1, 1, 1]
+        return out.squeeze(dim=2).squeeze(dim=2)
+
+
+class ModuleC(nn.Module):
+    def __init__(self):
+        super(ModuleC, self).__init__()
+        self.Conv_0 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1)
+        self.Conv_1 = nn.Conv2d(in_channels=3, out_channels=1, kernel_size=3, stride=1, padding=1)
+        self.Maxpool = nn.MaxPool2d(kernel_size=5, stride=1)
+
+    def forward(self, x):
+        out = self.Conv_0(x)#[B, 3, 5, 5]
+        out = self.Conv_1(out) # [B, 1, 5, 5]
+        out = self.Maxpool(out) # [B, 1, 1, 1]
         return out.squeeze(dim=2).squeeze(dim=2)
